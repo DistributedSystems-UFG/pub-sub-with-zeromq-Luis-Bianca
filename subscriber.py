05a5227b1991fc7e7796ed5e2b2a20c094d93498
lguilhermebarbosa <133176331+lguilhermebarbosa@users.noreply.github.com>
2023-07-02 20:34:32 -0300

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -2,28 +2,31 @@ import zmq
 from constPS import *
 
 context = zmq.Context()
-s = context.socket(zmq.SUB)
-p = "tcp://" + HOST + ":" + PORT
-s.connect(p)
+sub_socket = context.socket(zmq.SUB)
+sub_socket.connect(f"tcp://{HOST}:{PUB_PORT}")
+sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
+
+rpc_socket = context.socket(zmq.REQ)
+rpc_socket.connect(f"tcp://{HOST}:{RPC_PORT}")
 
 while True:
-    print("1. Receber mensagens individuais")
-    print("2. Receber mensagens de um tópico")
+    print("1. Receber mensagens individuais (RPC)")
+    print("2. Receber mensagens de um tópico (Pub-Sub)")
     choice = input("Escolha uma opção: ")
 
     if choice == "1":
         username = input("Digite o nome do seu usuário: ")
         topic = f"USER.{username}"
-        s.setsockopt_string(zmq.SUBSCRIBE, topic)
-        while True:
-            message = s.recv_string()
-            print(f"Mensagem individual recebida: {message}")
+        message = input("Digite a mensagem: ")
+        msg = f"{topic} {message}"
+        rpc_socket.send_string(msg)
+        response = rpc_socket.recv_string()
+        print("Resposta do servidor (RPC):", response)
     elif choice == "2":
         topic = input("Digite o nome do tópico: ")
         topic = f"TOPIC.{topic}"
-        s.setsockopt_string(zmq.SUBSCRIBE, topic)
         while True:
-            message = s.recv_string()
+            message = sub_socket.recv_string()
             print(f"Mensagem de tópico recebida: {message}")
     else:
         print("Opção inválida. Tente novamente.")
