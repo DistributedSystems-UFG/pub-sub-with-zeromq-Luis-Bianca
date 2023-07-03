2d8c9006c8c43985aa09866c0509bfb2f08adf3f
lguilhermebarbosa <133176331+lguilhermebarbosa@users.noreply.github.com>
2023-07-02 23:19:11 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -8,6 +8,7 @@ p = "tcp://" + HOST + ":" + PORT
 s.bind(p)
 
 while True:
+    # Exibe o menu de opções para o usuário
     print("Escolha uma opção:")
     print("1. Enviar mensagem individual")
     print("2. Enviar mensagem para tópico")
@@ -15,29 +16,22 @@ while True:
     option = input("Opção: ")
 
     if option == "1":
+        # Solicita ao usuário o nome do destinatário e a mensagem
         recipient = input("Digite o nome do destinatário: ")
         message = input("Digite a mensagem: ")
+        # Monta a mensagem usando "RPC" para identificar que é uma mensagem individual
         msg = str.encode(f"RPC {recipient} {message}")
+        # Envia a mensagem
         s.send(msg)
     elif option == "2":
+        # Solicita ao usuário o tópico e a mensagem
         topic = input("Digite o tópico: ")
         message = input("Digite a mensagem: ")
+        # Monta a mensagem usando "PUB" para identificar que é uma mensagem de tópico
         msg = str.encode(f"PUB {topic} {message}")
         s.send(msg)
     elif option == "3":
+        # Encerra a aplicação
         break
     else:
         print("Opção inválida. Por favor, tente novamente.")
-
-#import zmq, time
-#from constPS import * #-
-
-#context = zmq.Context()
-#s = context.socket(zmq.PUB)        # create a publisher socket
-#p = "tcp://"+ HOST +":"+ PORT      # how and where to communicate
-#s.bind(p)                          # bind socket to the address
-#while True:
-#	time.sleep(5)                    # wait every 5 seconds
-#	msg = str.encode("TIME " + time.asctime())
-#	s.send(msg) # publish the current time
-#
